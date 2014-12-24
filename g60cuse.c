cb7c2ff15ab3cdedfb40085793ef6b0312558637
Kevin Cernekee <cernekee@gmail.com>
2014-12-24 10:10:38 -0800

    Hardcode dev_name; remove more example code
    Currently this will create /dev/g60, and read back up to 0x1000 bytes that
    were written to the same node.
    
    Signed-off-by: Kevin Cernekee <cernekee@gmail.com>

--- g60cuse.c
+++ g60cuse.c
@@ -20,8 +20,9 @@
 #include <unistd.h>
 #include <errno.h>
 
+#define MAX_XFER		0x1000
+
 static void *cusexmp_buf;
-static size_t cusexmp_size;
 
 static const char *usage =
 "usage: cusexmp [options]\n"
@@ -33,33 +34,6 @@ static const char *usage =
 "    --name=NAME|-n NAME   device name (mandatory)\n"
 "\n";
 
-static int cusexmp_resize(size_t new_size)
-{
-	void *new_buf;
-
-	if (new_size == cusexmp_size)
-		return 0;
-
-	new_buf = realloc(cusexmp_buf, new_size);
-	if (!new_buf && new_size)
-		return -ENOMEM;
-
-	if (new_size > cusexmp_size)
-		memset(new_buf + cusexmp_size, 0, new_size - cusexmp_size);
-
-	cusexmp_buf = new_buf;
-	cusexmp_size = new_size;
-
-	return 0;
-}
-
-static int cusexmp_expand(size_t new_size)
-{
-	if (new_size > cusexmp_size)
-		return cusexmp_resize(new_size);
-	return 0;
-}
-
 static void cusexmp_open(fuse_req_t req, struct fuse_file_info *fi)
 {
 	fuse_reply_open(req, fi);
@@ -68,34 +42,29 @@ static void cusexmp_open(fuse_req_t req, struct fuse_file_info *fi)
 static void cusexmp_read(fuse_req_t req, size_t size, off_t off,
 			 struct fuse_file_info *fi)
 {
-	(void)fi;
-
-	if (off >= cusexmp_size)
-		off = cusexmp_size;
-	if (size > cusexmp_size - off)
-		size = cusexmp_size - off;
-
-	fuse_reply_buf(req, cusexmp_buf + off, size);
+	if (size > MAX_XFER)
+		size = MAX_XFER;
+	fuse_reply_buf(req, cusexmp_buf, size);
 }
 
 static void cusexmp_write(fuse_req_t req, const char *buf, size_t size,
 			  off_t off, struct fuse_file_info *fi)
 {
-	(void)fi;
-
-	if (cusexmp_expand(off + size)) {
-		fuse_reply_err(req, ENOMEM);
+	if (!cusexmp_buf)
+		cusexmp_buf = calloc(1, MAX_XFER);
+	if (!cusexmp_buf)
+		abort();
+	if (size > MAX_XFER) {
+		fuse_reply_err(req, ENOSPC);
 		return;
 	}
-
-	memcpy(cusexmp_buf + off, buf, size);
+	memcpy(cusexmp_buf, buf, size);
 	fuse_reply_write(req, size);
 }
 
 struct cusexmp_param {
 	unsigned		major;
 	unsigned		minor;
-	char			*dev_name;
 	int			is_help;
 };
 
@@ -106,8 +75,6 @@ static const struct fuse_opt cusexmp_opts[] = {
 	CUSEXMP_OPT("--maj=%u",		major),
 	CUSEXMP_OPT("-m %u",		minor),
 	CUSEXMP_OPT("--min=%u",		minor),
-	CUSEXMP_OPT("-n %s",		dev_name),
-	CUSEXMP_OPT("--name=%s",	dev_name),
 	FUSE_OPT_KEY("-h",		0),
 	FUSE_OPT_KEY("--help",		0),
 	FUSE_OPT_END
@@ -140,29 +107,20 @@ static const struct cuse_lowlevel_ops cusexmp_clop = {
 int main(int argc, char **argv)
 {
 	struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-	struct cusexmp_param param = { 0, 0, NULL, 0 };
-	char dev_name[128] = "DEVNAME=";
-	const char *dev_info_argv[] = { dev_name };
+	struct cusexmp_param param = { 0, 0, 0 };
 	struct cuse_info ci;
+	const char *dev_info_argv = "DEVNAME=g60";
 
 	if (fuse_opt_parse(&args, &param, cusexmp_opts, cusexmp_process_arg)) {
 		printf("failed to parse option\n");
 		return 1;
 	}
 
-	if (!param.is_help) {
-		if (!param.dev_name) {
-			fprintf(stderr, "Error: device name missing\n");
-			return 1;
-		}
-		strncat(dev_name, param.dev_name, sizeof(dev_name) - 9);
-	}
-
 	memset(&ci, 0, sizeof(ci));
 	ci.dev_major = param.major;
 	ci.dev_minor = param.minor;
 	ci.dev_info_argc = 1;
-	ci.dev_info_argv = dev_info_argv;
+	ci.dev_info_argv = &dev_info_argv;
 	ci.flags = CUSE_UNRESTRICTED_IOCTL;
 
 	return cuse_lowlevel_main(args.argc, args.argv, &ci, &cusexmp_clop,
